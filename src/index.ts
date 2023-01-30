cd1cb34897f8e635f4b39a1175bcf9f3dc4f5524
Randy Otero Jimenez <rotero@espinolabs.fr>
2023-01-30 08:44:25 +0000

    Add User class

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log("Hi there")
+import { User } from "./User";
+
+const user = new User({ name: " Pedro", age: 75 })
+
+user.set({ name: 'Marilyn', age: 65 })
+
+user.on('change', () => console.log('change event triggered'))
+user.on('update', () => console.log('update event triggered'))
+
+user.trigger('change')
+user.trigger('update')
+
+console.log(user)
